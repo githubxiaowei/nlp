787d500bc1937b2d46e8d5e907f7e741d0156552
234 <@>
2017-12-06 23:36:01 +0800

    walawala

--- 分词感知机/perceptron.py
+++ 分词感知机/perceptron.py
@@ -2,11 +2,30 @@
 """
 Created on Wed Dec  6 22:21:49 2017
 
-@author: DellT5810
+@author: https://github.com/githubxiaowei
 """
 
-with open('train.txt',encoding='utf8') as f:
-    data = f.readlines()
-    
-print(data[0].strip('\n').split('  '))
+def parse(str):
+    target = ''
+    flag = 0
+    for c in str:
+        if(c==' '):
+            flag += 1
+            continue
+        if(flag==2):
+            target += '1'
+            flag = 0
+        else:
+            target += '0'
+    return target
+
+train_x,train_y = [],[]
+with open('train.txt',encoding='utf8') as f:  
+    for line in f.readlines():
+        train_x.append(line[:-1].replace('  ',''))
+        train_y.append(parse(line[:-1]))
+
+
+
+
 
